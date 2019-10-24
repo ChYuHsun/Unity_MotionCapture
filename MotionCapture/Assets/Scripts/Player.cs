925a3d806eb88df2d143cbab3c921f447d908fb4
ChYuHsun <51938539+ChYuHsun@users.noreply.github.com>
2019-10-24 10:28:32 +0800

    complet animation setting

--- MotionCapture/Assets/Scripts/Player.cs
+++ MotionCapture/Assets/Scripts/Player.cs
@@ -2,7 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
 
+public class Player : MonoBehaviour
+{ 
+
+    Animator Robot;                                  //設定名稱
+
+    private void Start()
+    {
+        Robot = GetComponent<Animator>();            //抓取Animator
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))         //按下space攻擊
+        {
+            Robot.SetTrigger("toBigAttack");
+        }
+
+        if(Input.GetKey(KeyCode.R))                  //按下R奔跑
+        {
+            Robot.SetBool("toRun", true);
+        }
+        else                                         //放開停下奔跑
+        {
+            Robot.SetBool("toRun", false);
+        }
+
+        //優化寫法
+
+        //if(Input.GetKeyDown(KeyCode.Space)) Robot.SetTrigger("toBigAttack");
+
+        //Robot.SetBool("toRun", Input.GetKey(KeyCode.R));
+    }
 }
